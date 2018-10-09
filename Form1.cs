c42546f192b50c048458756a27093890e893607d
Hong Duc <01627dlm@gmail.com>
2018-10-09 14:51:57 +0700

    update dieu kien

--- Form1.cs
+++ Form1.cs
@@ -18,10 +18,13 @@ namespace demoQlNhanVien
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(txtA.Text);
-            double b = double.Parse(txtB.Text);
-            double c = a + b;
-            txtKetQua.Text = c.ToString();
+            if (txtA.Text != "" && txtB.Text != "")
+            {
+                double a = double.Parse(txtA.Text);
+                double b = double.Parse(txtB.Text);
+                double c = a + b;
+                txtKetQua.Text = c.ToString();
+            }
         }
     }
 }
